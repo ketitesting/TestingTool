ed776c58eb5f8c9174faa30a7f3342c469ea1564
KETI <novamartin@keti.re.kr>
2015-11-04 19:21:42 +0900

    Print a welcome message for joing conformance testing tool development

---
+++ HelloTesting/src/kr/re/keti/testing/HelloTesting.java
@@ -0,0 +1,13 @@
+package kr.re.keti.testing;
+
+public class HelloTesting {
+/**
+ * @author keti-m
+ * @param args
+ */
+	public static void main(String[] args) {
+		System.out.println("Welcome to join onem2m conformance testing tool developement^^");
+
+	}
+
+}
