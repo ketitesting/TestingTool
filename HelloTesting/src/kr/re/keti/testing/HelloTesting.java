3afb08511efd73d63c5bd92fda6dedd70b88b732
KetiTesting <novamartin@keti.re.kr>
2015-11-04 22:55:11 +0900

    Testing framework discussion

--- HelloTesting/src/kr/re/keti/testing/HelloTesting.java
+++ HelloTesting/src/kr/re/keti/testing/HelloTesting.java
@@ -7,7 +7,7 @@ public class HelloTesting {
  */
 	public static void main(String[] args) {
 		System.out.println("Welcome to join onem2m conformance testing tool developement^^");
-
+		System.out.println("A testing framework needs to be decided first!");
 	}
 
 }
